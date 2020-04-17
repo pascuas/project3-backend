c277588c67bf9a36cddb66eb5d6480b0e25f9c08
Alyssa Jackson <alyssajackson@rescomp-19-388097.stanford.edu>
2020-04-17 14:15:50 -0700

    controllers

---
+++ controllers/presentation.js
@@ -0,0 +1,28 @@
+const Presentation = require('../models/Presentation')
+
+const getAll = (req, res) => {
+    Presentation.find().then(prez => res.json(prez)).then(()=> process.exit())
+}
+
+const getById = (req, res) => {
+    Presentation.findById(req.params.id).then(prez => res.json(prez)).then(()=> process.exit())
+}
+
+const create = (req, res) => {
+    Presentation.create(req.body).then(prez => res.json(prez)).then(()=> process.exit())
+}
+const update = (req, res) => {
+    Presentation.findByIdAndUpdate(req.params.id, req.body).then(prez => res.json(prez)).then(()=> process.exit())
+}
+
+const remove = (req, res) => {
+    Presentation.remove(req.params.id).then(prez => res.json(prez)).then(()=> process.exit())
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+}
