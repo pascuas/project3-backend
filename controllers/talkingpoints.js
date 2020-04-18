a468bbf9e99c78c90deff67a00cfc5713c07fad4
marshall brock <marshallbrock@gmail.com>
2020-04-17 20:06:27 -0500

    fixed spelling

--- controllers/talkingpoints.js
+++ controllers/talkingpoints.js
@@ -1,26 +1,26 @@
 const Talkingpoints = require('../models/presentation')
 
-
+//marshall was here
 const getAll = (req, res) => {
-    Talkingpoints.Talkingpoints.find({}).then(talkingpoints=>{
+    Talkingpoints.TalkingPoint.find({}).then(talkingpoints=>{
         res.json(talkingpoints)
     })
 }
 
 const getById = (req, res) => {
-    Talkingpoints.Talkingpoints.findById(req.params.id).then(talkingpoints => res.json(talkingpoints))
+    Talkingpoints.TalkingPoint.findById(req.params.id).then(talkingpoints => res.json(talkingpoints))
 }
 
 const create = (req, res) => {
-    Talkingpoints.Talkingpoints.create(req.body).then(talkingpoints =>res.json(talkingpoints))
+    Talkingpoints.TalkingPoint.create(req.body).then(talkingpoints =>res.json(talkingpoints))
 }
 
 const update = (req, res) => {
-    Talkingpoints.Talkingpoints.findByIdAndUpdate(req.params.id, req.body).then(talkingpoints => res.json(talkingpoints))
+    Talkingpoints.TalkingPoint.findByIdAndUpdate(req.params.id, req.body).then(talkingpoints => res.json(talkingpoints))
 }
 
 const remove = (req, res) => {
-    Talkingpoints.Talkingpoints.remove({_id: req.params.id}).then(talkingpoints=> res.json(talkingpoints))
+    Talkingpoints.TalkingPoint.remove({_id: req.params.id}).then(talkingpoints=> res.json(talkingpoints))
 }
 
 module.exports ={
