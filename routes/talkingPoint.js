ebd482daf12b8215ab0fb247fde28e6717b682f4
Stanley Pascua <stanleypascua@Stanleys-MBP.lan1>
2020-04-20 14:57:45 -0700

    updating talkpoint controller

--- routes/talkingPoint.js
+++ routes/talkingPoint.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const controller = require('../controllers/talkingpoints')
-// router.get('/', controller.getAll)
-// router.get('/:id', controller.getById)
+router.get('/', controller.getAll)
+router.get('/:id', controller.getById)
 router.post('/:sectId', controller.create)
-// router.put('/:presId/:sectId/:pointId', controller.update)
-// router.delete('/:sectId/:pointId', controller.removeAll)
-router.delete('/:presId/:sectId/:pointId', controller.remove)
+router.put('/:pointId', controller.update)
+router.delete('/', controller.removeAll)
+router.delete('/:pointId', controller.remove)
 
 
 module.exports = router
